9882684688bc28efe4d28602828ef4c694330c61
vanessaCifuentes <vane-9@live.cl>
2019-01-17 13:25:03 -0300

    avance 17-1

--- proyecto_codigo/src/main/java/cl/accenture/programatufuturo/proyecto/DAO/TipoDAO.java
+++ proyecto_codigo/src/main/java/cl/accenture/programatufuturo/proyecto/DAO/TipoDAO.java
@@ -1,11 +1,13 @@
 package cl.accenture.programatufuturo.proyecto.DAO;
 
 import cl.accenture.programatufuturo.proyecto.exception.SinConexionException;
+import cl.accenture.programatufuturo.proyecto.model.Estado;
 import cl.accenture.programatufuturo.proyecto.model.Reclamo;
 import cl.accenture.programatufuturo.proyecto.model.Tipo;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,98 +19,140 @@ public class TipoDAO {
     public TipoDAO(Conexion conexion) {
         this.conexion = conexion;
     }
+
     //metodo insertar reclamo
-    public void insertarTiporeclamo(Tipo a)throws SinConexionException {
+
+
+    public void insertarTiporeclamo(Tipo a) throws SinConexionException {
         try {
             PreparedStatement pstatementInsert = conexion.getConexion().prepareStatement("INSERT INTO tiporeclamo(Id,nombre,SLA");
             pstatementInsert.setInt(1, a.getId());
             pstatementInsert.setString(2, a.getNombre());
             pstatementInsert.setInt(3, a.getSla());
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
 
         }
     }
+
     //metodo para buscar tiporeclamo por id
-    public void buscarporid(Integer a)throws SinConexionException{
-        try{
-            PreparedStatement pstatementSelect = conexion.getConexion().prepareStatement("SELECT * FROM tiporeclamo WHERE Id=?");
-            pstatementSelect.setInt(1, a);
-            ResultSet rs = pstatementSelect.executeQuery();
 
+
+    public List<Tipo>buscartipoconid (Integer t) throws SinConexionException {
+        List<Tipo> tipos = new ArrayList<Tipo>();    //creo una lista y la inicializo como Arraylist
+        try {
+            final String SQL = "SELECT * FROM tiporeclamo WHERE Id=?";
+            PreparedStatement ps = conexion.getConexion().prepareStatement(SQL);
+            ps.setInt(1, t);
+            ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                System.out.println(rs.getInt("Id"));
-                System.out.println(rs.getString("nombre"));
-                System.out.println(rs.getInt("SLA"));
+                Tipo tipo = new Tipo();
+                tipo.setId(rs.getInt(1));
+                tipo.setNombre(rs.getString(2));
+                tipos.add(tipo);
             }
-        }catch (SQLException e){
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return tipos;
+    }
+
+
+    //metodo para buscar por nombre
+
+    public List<Tipo> buscarpornombre(String nombre) throws SinConexionException {
+        List<Tipo> tipos = new ArrayList<Tipo>();
+        try {
+            final String SQL = "SELECT * FROM tiporeclamo WHERE nombre=?";
+            PreparedStatement ps = conexion.getConexion().prepareStatement(SQL);
+            ps.setString(1, nombre);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Tipo tipo = new Tipo();
+                tipo.setId(rs.getInt(1));
+                tipo.setNombre(rs.getString(2));
+                tipo.setSla(rs.getInt(3));
+                tipos.add(tipo);
+            }
+        } catch (SQLException o) {
+            o.printStackTrace();
+        }
+        return tipos;
+    }
+
+
+    //metodo para ordenar por SLA
+
+
+    public List<Tipo> ordenarporsla() {
+        List<Tipo> tipos = new ArrayList<Tipo>();
+        try {
+            final String SQL = "SELECT * FROM tipoReclamo WHERE ORDER BY sla ASC";
+            PreparedStatement ps = conexion.getConexion().prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Tipo tipo = new Tipo();
+                tipo.setId(rs.getInt(1));
+                tipo.setNombre(rs.getString(2));
+                tipo.setSla(rs.getInt(3));
+                tipos.add(tipo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (SinConexionException e) {
             e.printStackTrace();
         }
+        return tipos;
     }
-    //metodo para buscar por nombre
-    public void buscarpornombre(String nombre)throws SinConexionException{
-        try{
-            PreparedStatement pstatementSelect = conexion.getConexion().prepareStatement("SELECT * FROM permiso WHERE nombre=? ");
-            pstatementSelect.setString (1, nombre);
-            ResultSet rs = pstatementSelect.executeQuery();
-            while(rs.next()){
-                System.out.print(rs.getInt("Id"));
-                System.out.print(rs.getString("nombre"));
-                System.out.print(rs.getInt("SLA"));
-                System.out.println("");
+
+
+    //metodo para buscar por SLA
+
+
+    public List<Tipo> buscarporsla(Integer sla) throws SinConexionException {
+        List<Tipo> tipos = new ArrayList<Tipo>();
+        try {
+            final String SQL = "SELECT * FROM tiporeclamo WHERE SLA=?";
+            PreparedStatement ps = conexion.getConexion().prepareStatement(SQL);
+            ps.setInt(1, sla);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Tipo tipo = new Tipo();
+                tipo.setId(rs.getInt(1));
+                tipo.setNombre(rs.getString(2));
+                tipo.setSla(rs.getInt(3));
+                tipos.add(tipo);
             }
-            statement.close();
-            rs.close();
-        }catch (SQLException o){
+        } catch (SQLException o) {
             o.printStackTrace();
         }
+        return tipos;
     }
-//metodo para ordenar por SLA
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //metodo para buscar por SLA ayudaaaa
-    public List<Reclamo> buscarporsla(Reclamo p)throws SinConexionException{
-        List<Reclamo>reclamos=new ArrayList<Reclamo>();
-        try{
-            statement =conexion.getConexion().createStatement();
-            String query="SELECT * FROM tiporeclamo WHERE nombre=?";
-            PreparedStatement statement=conexion.getConexion().prepareStatement(query);
-            statement.setArray(3,p);
-            ResultSet rs=statement.executeQuery();
-            while(rs.next()){
-                System.out.print(rs.getInt("Id"));
-                System.out.print(rs.getString("nombre"));
-                System.out.print(rs.getInt("SLA"));
-                System.out.println("");
-            }
-            statement.close();
-            rs.close();
-        }catch (SQLException o) {
-            o.printStackTrace();
-        }
-        return reclamos;
-
-    }
     //metodo para ordenar por nombre
 
 
-
-
+    public List<Tipo> ordenarpornombre() {
+        List<Tipo> tipos = new ArrayList<Tipo>();
+        try {
+            final String SQL = "SELECT * FROM tipoReclamo WHERE ORDER BY nombre ASC";
+            PreparedStatement ps = conexion.getConexion().prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Tipo tipo = new Tipo();
+                tipo.setId(rs.getInt(1));
+                tipo.setNombre(rs.getString(2));
+                tipo.setSla(rs.getInt(3));
+                tipos.add(tipo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (SinConexionException e) {
+            e.printStackTrace();
+        }
+        return tipos;
+    }
 }
+
+
--- proyecto_codigo/src/main/java/cl/accenture/programatufuturo/proyecto/model/Tipo.java
+++ proyecto_codigo/src/main/java/cl/accenture/programatufuturo/proyecto/model/Tipo.java
@@ -4,15 +4,27 @@ public class Tipo {
 
     private Integer id;
     private String nombre;
-    private int sla;
+    private Integer sla;
 
-    public Tipo(int id, String nombre, int sla) {
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSla(Integer sla) {
+        this.sla = sla;
+    }
+
+    public Tipo(Integer id, String nombre, Integer sla) {
         this.id = id;
         this.nombre = nombre;
         this.sla = sla;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
@@ -20,7 +32,10 @@ public class Tipo {
         return nombre;
     }
 
-    public int getSla() {
+    public Integer getSla() {
         return sla;
+    }
+    public Tipo(){
+
     }
 }
